7d4f87b16e3d4831469b19f5e99e5ba2530e60f7
Jairo221451 <221451@unsaac.edu.pe>
2025-06-24 21:44:23 -0500

    Implementacion de ventaService.js

---
+++ Frontend/src/Services/ventaService.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:3001/api/ventas';
+
+export const getVentas = () => axios.get(API_URL);
+export const getVentaById = (id) => axios.get(`${API_URL}/${id}`);
+export const createVenta = (ventaData) => axios.post(API_URL, ventaData);
